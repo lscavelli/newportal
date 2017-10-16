a30138495ac1d3c247cea3ac7851faf7bbf8cfd4
LFG Scavelli <newportalclub@gmail.com>
2017-10-16 15:42:43 +0200

    fixing error access db

--- app/Providers/PortletServiceProvider.php
+++ app/Providers/PortletServiceProvider.php
@@ -26,36 +26,41 @@ class PortletServiceProvider extends ServiceProvider
     {
         $portlet = $this->app->make('portlet');
         $dir = app_path().'/'.config("newportal.portlets.namespace").'/';
-        if(is_dir($dir) and (\Illuminate\Support\Facades\Schema::hasTable('portlets'))) {
-            foreach ($portlet->listPagePortlets() as $portlet) {
-                $arrayName = explode("\\", $portlet->path);
-                $nameAuthor = reset($arrayName);
-                $namePortlet = next($arrayName);
-                $path = $dir . $nameAuthor."/".$namePortlet;
 
-                if (!File::isDirectory($path)) continue;
-                if (!$this->app->routesAreCached()) {
+        try {
+            if(is_dir($dir) and (\Illuminate\Support\Facades\Schema::hasTable('portlets'))) {
+                foreach ($portlet->listPagePortlets() as $portlet) {
+                    $arrayName = explode("\\", $portlet->path);
+                    $nameAuthor = reset($arrayName);
+                    $namePortlet = next($arrayName);
+                    $path = $dir . $nameAuthor."/".$namePortlet;
 
-                    $routes = [
-                        $path . '/routes/web.php',
-                        $path . '/routes/console.php',
-                        $path . '/routes/api.php',
-                        $path . '/routes/channels.php',
-                    ];
-                    foreach ($routes as $route) {
-                        if (File::exists($route)) $this->loadRoutesFrom($route);
+                    if (!File::isDirectory($path)) continue;
+                    if (!$this->app->routesAreCached()) {
+
+                        $routes = [
+                            $path . '/routes/web.php',
+                            $path . '/routes/console.php',
+                            $path . '/routes/api.php',
+                            $path . '/routes/channels.php',
+                        ];
+                        foreach ($routes as $route) {
+                            if (File::exists($route)) $this->loadRoutesFrom($route);
+                        }
                     }
-                }
-                if (File::exists($path . '/helper.php')) include_once $path . '/helper.php';
-                if (File::isDirectory($path . '/views')) $this->loadViewsFrom($path . '/views', $namePortlet);
-                if (File::isDirectory($path . '/translations')) $this->loadTranslationsFrom($path . '/translations', $namePortlet);
-                if (File::isDirectory($path . '/assets')) {
-                    //$this->publishes([
-                    //    $path . '/assets' => sprintf("%s/%s/", public_path(), strtolower(config('newportal.portlets.namespace'))).$nameAuthor."/".$namePortlet,
-                    //], 'public');
-                }
+                    if (File::exists($path . '/helper.php')) include_once $path . '/helper.php';
+                    if (File::isDirectory($path . '/views')) $this->loadViewsFrom($path . '/views', $namePortlet);
+                    if (File::isDirectory($path . '/translations')) $this->loadTranslationsFrom($path . '/translations', $namePortlet);
+                    if (File::isDirectory($path . '/assets')) {
+                        //$this->publishes([
+                        //    $path . '/assets' => sprintf("%s/%s/", public_path(), strtolower(config('newportal.portlets.namespace'))).$nameAuthor."/".$namePortlet,
+                        //], 'public');
+                    }
 
+                }
             }
+        } catch (\Exception $e) {
+            //
         }
     }
 
